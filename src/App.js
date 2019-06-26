ebdb3dfc4bee7e59fe3dc5d9b8c75d477067d0fc
Eskil Hognestad <eskilhognestad@Eskils-MacBook-Pro.local>
2019-06-26 13:39:08 +0200

    initial react functionality for our smooth application

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+    render(){
+        console.log("hei dette funker");
+        return(
+            <div className= "App">
+                <h1>Halla balla</h1>
+              
+                
+            </div>
+        );
+    }
+}
+
+export default App;
