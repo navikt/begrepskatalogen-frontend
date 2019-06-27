09a31ac536c1c49737bfc60df04da70e06522a14
Michael Johansen <michael.johansen@nav.no>
2019-06-27 09:41:20 +0200

    First version of the mock bugfix (#7)

---
+++ devserver.js
@@ -0,0 +1,15 @@
+const Bundler = require('parcel-bundler');
+const app = require('express')();
+const mockApi = require('./test-data/mock');
+const file = 'src/index.html'; // Pass an absolute path to the entrypoint here
+const options = {}; // See options section of api docs, for the possibilities
+
+// Initialize a new bundler using a file and options
+const bundler = new Bundler(file, options);
+
+// Let express use the bundler middleware, this will let Parcel handle every request over your express server
+
+mockApi(app);
+app.use(bundler.middleware());
+// Listen on port 1234
+app.listen(1234);
