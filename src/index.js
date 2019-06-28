bf2846013f2864121997ae7e0bb83906d9fa3d44
Thea Tokstad <t156627@Theas-MacBook-Pro.local>
2019-06-28 12:33:44 +0200

    #19 added redux in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(
+    <Provider store={ store } >
+        <App/>
+    </Provider>, 
+    document.getElementById('app'));
