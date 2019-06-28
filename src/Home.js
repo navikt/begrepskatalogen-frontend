bcba9dc62e701fb5d9b89b9d23f6140aa2841c71
Eskil Hognestad <eskilhognestad@Eskils-MacBook-Pro.local>
2019-06-28 14:56:24 +0200

    routing works. added functionality to til toppen-button in footer, changed content to Home.js and removed it from App. More logic structure I guess?

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Table from './components/table/Table';
+
+import FilterField from './components/FilterSelectField/FilterField';
+import SortField from './components/SortSelectField/SortField';
+
 
 export class Home extends Component {
     render() {
         return (
             <div>
+
                 <h1>homepage</h1>
+                <FilterField/><SortField/>
+                <Table/>
+                
+
             </div>
         );
     }
