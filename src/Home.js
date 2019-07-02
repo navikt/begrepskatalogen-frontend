575448691d95170bc54a9096c1de9f590c57acd7
Eskil Hognestad <eskilhognestad@Eskils-MacBook-Pro.local>
2019-07-02 10:19:16 +0200

    hiding the table until click is now functional.

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import Table from './components/table/Table';
-
+import { Ingress } from 'nav-frontend-typografi';
 import FilterField from './components/FilterSelectField/FilterField';
 import SortField from './components/SortSelectField/SortField';
+import './Home.less';
 
 
 export class Home extends Component {
+    constructor(){
+        super();
+        this.state = {
+            isHidden: true
+        }
+    }
+    
+    toggleHidden(){
+        this.setState({
+            isHidden: !this.state.isHidden
+        })
+    }
+
     render() {
         return (
             <div>
 
-                <h1>homepage</h1>
+                <Ingress>Søk etter et begrep, stykkord eller 
+                    <button onClick = {this.toggleHidden.bind(this)}>
+                        se alle begrepene</button> </Ingress>
+                <br/>
                 <FilterField/><SortField/>
-                <Table/>
+                {!this.state.isHidden && <Table/>}
                 
 
             </div>
