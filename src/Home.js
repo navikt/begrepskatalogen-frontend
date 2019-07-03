f3730ab8c46f99abc716b0c5e684c2d79d04384d
Dennis <dennis.jianbin.liang@nav.no>
2019-07-03 12:09:26 +0200

    removes unused importstatement

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Table from './components/table/Table';
 import { Ingress } from 'nav-frontend-typografi';
-import FilterField from './components/FilterSelectField/FilterField';
-import SortField from './components/SortSelectField/SortField';
 import './Home.less';
 
 
