dd9bd8b391ce712c6cfa2d7dacec550f333e55a2
Eskil Hognestad <eskilhognestad@Eskils-MacBook-Pro.local>
2019-07-03 11:18:30 +0200

    changed footer elements from buttons to <Element>

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Flatknapp } from 'nav-frontend-knapper';
 import './Footer.less';
 import ScrollUpButton from 'react-scroll-up-button';
+import { Element } from 'nav-frontend-typografi';
 
 class Footer extends React.Component {
     render() {
@@ -11,9 +12,9 @@ class Footer extends React.Component {
             //Todo: LEgge til router så knappene fungerer. 
             <div className="footer">
                 <div className="footerElements">
-                    <ScrollUpButton><Flatknapp>Til toppen</Flatknapp></ScrollUpButton>
-                    <Flatknapp>Søk i katalogen</Flatknapp>
-                    <Flatknapp>Meld inn nytt begrep</Flatknapp>
+                    <ScrollUpButton><Element className="element">Til toppen</Element></ScrollUpButton>
+                    <Element className="element">Søk i katalogen</Element>
+                    <Element className="element">Meld inn nytt begrep</Element>
                 </div>
             </div>
 
