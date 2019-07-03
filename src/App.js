b52128b01951ae8a3e0964012c67cde199540a94
Eskil Hognestad <eskilhognestad@Eskils-MacBook-Pro.local>
2019-07-03 13:55:31 +0200

    started on termpage

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Table from './components/table/Table';
 import { connect } from 'react-redux';
 import Home from './Home';
 import KontaktOss from './KontaktOss';
+import TermPage from './TermPage';
 
 const mapStateToProps = (store) => ({
     fancy: store.fancy
@@ -34,6 +35,7 @@ class App extends Component{
             
                     <Route exact path = '/' component = {Home} />
                     <Route exact path='/kontaktoss' component = {KontaktOss}/>
+                    <Route exact path = '/begrepsside' component = {TermPage}/>
                     <Footer/>
 
                 </Router>
---
+++ src/TermPage.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Sidetittel, Ingress, Undertittel, Normaltekst} from 'nav-frontend-typografi';
+import { Link } from 'nav-frontend-knapper';
+import './TermPage.less';
+
+export class TermPage extends React.Component{
+
+    render(){
+        return(
+            <div className="gridContainer">
+            <div className="TermPage">
+                <div className="begrepsHeader">
+                    <Normaltekst className="status">Status</Normaltekst>
+                    <Normaltekst className="linker">Del begrepet</Normaltekst>
+                    <Normaltekst className="linker">Gi innspill til begrepet</Normaltekst>
+                </div>
+                <div className="venstreFeltAvSiden">
+                    <div className = "begrepsoverskrift">
+                        <Sidetittel>Begrep</Sidetittel>
+                        <br/>
+                        <Undertittel>Kort beskrivelse av begrep</Undertittel>
+                    </div>
+                    <hr/>
+                    <div className = "begrepsforklaring">
+                        <Ingress>Begrepsforklaring</Ingress>
+                        <Normaltekst>Konkret forklaring på begrepet</Normaltekst>
+                    </div>
+                    <div className = "eksempler">
+                        <Ingress>Eksempler</Ingress>
+                        <Normaltekst>Konkrete eksempler</Normaltekst>
+                    </div>
+                    <div className="kilde">
+                        <Ingress>Kilde</Ingress>
+                        <Normaltekst>xxxx</Normaltekst>
+                    </div>
+
+                    <div className ="relasjoner">
+                        <Ingress>Relasjon til andre begreper (Relasjonstre?)</Ingress>
+                        <p>xxxxx . xxxxx</p>
+                    </div>
+
+                    <div className="revidert">
+                        <Ingress>Revidert</Ingress>
+                        <p>xxx-xxxx-xxxxxx-o</p>
+                    </div>
+                </div>
+                <div className = "hoyreFeltAvSiden">
+                    <div className="beskrivelsestype">
+                        <Ingress>Beskrivelsestype</Ingress>
+                        <Normaltekst>Faglig definisjon</Normaltekst>
+                    </div>
+
+                    <div className = "fagomraade">
+                        <Ingress>Fagområde</Ingress>
+                        <Normaltekst>xxxxcccxcxcxxxxx</Normaltekst>
+                    </div>
+
+                    <div className = "begrepseier">
+                        <Ingress>Begrepseier</Ingress>
+                        <Normaltekst>Avdeling osv .. <br/> med mer</Normaltekst>
+                    </div>
+                    
+                </div>
+
+
+
+            </div>
+            </div>
+        );
+    }
+}
+
+export default TermPage;
