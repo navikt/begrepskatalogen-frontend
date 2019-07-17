8d4a90afd99e101a9de6a93e99ffd674471436a4
eskilsen <eskil_hognestad@hotmail.com>
2019-07-17 10:31:03 +0200

    Feat/term page eskil (#64)
    * small additions for godkjent-ikkegodkjent field in termpage
    
    * adds redux in termpage
    
    * this commit gives right functionality for termpage. needs name refactoring

--- src/redux/actions/AppActions.js
+++ src/redux/actions/AppActions.js
@@ -54,4 +54,13 @@ export const sortBy = (sort) => (dispatch) =>{
     })
 }
 
+export const termKey = (newKey) => {
+    return {
+        type: 'TERM_KEY',
+        termKey: newKey, 
+    
+    }
+}
+
+
 
