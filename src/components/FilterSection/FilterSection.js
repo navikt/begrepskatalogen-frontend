d23ec952b3f9b142acd783952d50dbfade58588b
Dennis <dennis.jianbin.liang@nav.no>
2019-07-18 14:44:36 +0200

    changes function filtersection to class

--- src/components/FilterSection/FilterSection.js
+++ src/components/FilterSection/FilterSection.js
@@ -4,68 +4,70 @@ import './FilterSection.less';
 import { Undertittel, Systemtittel } from 'nav-frontend-typografi';
 
 
-function FilterSection(){
-    return(
-        <div className="filterwrapper">
-            
-            <div className="Overskrift">
-                <Systemtittel>Filtrer etter kategori</Systemtittel>
+class FilterSection extends React.Component{
+    render(){
+        return(
+            <div className="filterwrapper">
+                
+                <div className="Overskrift">
+                    <Systemtittel>Filtrer etter kategori</Systemtittel>
+                </div>
+
+                <div className="katergorioverskrift">
+                    <Undertittel>Implisitt status</Undertittel>
+                </div>
+
+                <div className="filtercheckbox">
+                    <Checkbox label={"Godkjente"}/>
+                    <Checkbox label={"Utkast"}/>
+                    <Checkbox label={"Avviste"}/>
+                </div>
+
+                <div className="katergorioverskrift">
+                    <Undertittel>Fagområder</Undertittel>
+                </div>
+
+                <div className="filtercheckbox">
+                    <Checkbox label={"Fagområde1"}/>
+                    <Checkbox label={"Fagområde2"}/>
+                    <Checkbox label={"Fagområde3"}/>
+                </div>
+
+                <div className="katergorioverskrift">
+                    <Undertittel>Kildetype</Undertittel>
+                </div>
+
+                <div className="filtercheckbox">
+                    <Checkbox label={"Kildetype1"}/>
+                    <Checkbox label={"Kildetype2"}/>
+                    <Checkbox label={"Kildetype3"}/>
+                </div>
+
+
+                <div className="katergorioverskrift">
+                    <Undertittel>Ekstern eierskap?</Undertittel>
+                </div>
+
+                <div className="filtercheckbox">
+                    <Checkbox label={"Ja"}/>
+                    <Checkbox label={"Nei"}/>
+                </div>
+
+
+                <div className="katergorioverskrift">
+                    <Undertittel>Offentlig Status</Undertittel>
+                </div>
+
+                <div className="filtercheckbox">
+                    <Checkbox label={"Ja"}/>
+                    <Checkbox label={"Nei"}/>
+                </div>
+
+
+
             </div>
-
-            <div className="katergorioverskrift">
-                <Undertittel>Implisitt status</Undertittel>
-            </div>
-
-            <div className="filtercheckbox">
-                <Checkbox label={"Godkjente"}/>
-                <Checkbox label={"Utkast"}/>
-                <Checkbox label={"Avviste"}/>
-            </div>
-
-            <div className="katergorioverskrift">
-                <Undertittel>Fagområder</Undertittel>
-            </div>
-
-            <div className="filtercheckbox">
-                <Checkbox label={"Fagområde1"}/>
-                <Checkbox label={"Fagområde2"}/>
-                <Checkbox label={"Fagområde3"}/>
-            </div>
-
-            <div className="katergorioverskrift">
-                <Undertittel>Kildetype</Undertittel>
-            </div>
-
-            <div className="filtercheckbox">
-                <Checkbox label={"Kildetype1"}/>
-                <Checkbox label={"Kildetype2"}/>
-                <Checkbox label={"Kildetype3"}/>
-            </div>
-
-
-            <div className="katergorioverskrift">
-                <Undertittel>Ekstern eierskap?</Undertittel>
-            </div>
-
-            <div className="filtercheckbox">
-                <Checkbox label={"Ja"}/>
-                <Checkbox label={"Nei"}/>
-            </div>
-
-
-            <div className="katergorioverskrift">
-                <Undertittel>Offentlig Status</Undertittel>
-            </div>
-
-            <div className="filtercheckbox">
-                <Checkbox label={"Ja"}/>
-                <Checkbox label={"Nei"}/>
-            </div>
-
-
-
-        </div>
-    );
+        );
+    }
 }
 
 export default FilterSection;
