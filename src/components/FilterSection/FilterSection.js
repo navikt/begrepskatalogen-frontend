4dfb54dc09aa2a41fedf5e5e484d2c69c6b51e4d
Dennis <dennis.jianbin.liang@nav.no>
2019-07-18 12:42:50 +0200

    adds temporary style to filtersection. design not optimal

--- src/components/FilterSection/FilterSection.js
+++ src/components/FilterSection/FilterSection.js
@@ -1,60 +1,64 @@
 import React from 'react';
 import { Checkbox} from 'nav-frontend-skjema';
 import './FilterSection.less';
-import { Normaltekst } from 'nav-frontend-typografi';
+import { Undertittel, Systemtittel } from 'nav-frontend-typografi';
 
 
 function FilterSection(){
     return(
         <div className="filterwrapper">
             
-            <div className="overskrift">
-                <Normaltekst>Implisitt status</Normaltekst>
+            <div className="Overskrift">
+                <Systemtittel>Filtrer etter kategori</Systemtittel>
             </div>
 
-            <div>
+            <div className="katergorioverskrift">
+                <Undertittel>Implisitt status</Undertittel>
+            </div>
+
+            <div className="filtercheckbox">
                 <Checkbox label={"Godkjente"}/>
                 <Checkbox label={"Utkast"}/>
                 <Checkbox label={"Avviste"}/>
             </div>
 
-            <div className="overskrift">
-                <Normaltekst>Fagområder</Normaltekst>
+            <div className="katergorioverskrift">
+                <Undertittel>Fagområder</Undertittel>
             </div>
 
-            <div>
-                <Checkbox label={"Fagområde1'"}/>
-                <Checkbox label={"Fagområde2'"}/>
-                <Checkbox label={"Fagområde3'"}/>
+            <div className="filtercheckbox">
+                <Checkbox label={"Fagområde1"}/>
+                <Checkbox label={"Fagområde2"}/>
+                <Checkbox label={"Fagområde3"}/>
             </div>
 
-            <div className="overskrift">
-                <Normaltekst>Kildetype</Normaltekst>
+            <div className="katergorioverskrift">
+                <Undertittel>Kildetype</Undertittel>
             </div>
 
-            <div>
-                <Checkbox label={"Kildetype1'"}/>
-                <Checkbox label={"Kildetype2'"}/>
-                <Checkbox label={"Kildetype3'"}/>
+            <div className="filtercheckbox">
+                <Checkbox label={"Kildetype1"}/>
+                <Checkbox label={"Kildetype2"}/>
+                <Checkbox label={"Kildetype3"}/>
             </div>
 
 
-            <div className="overskrift">
-                <Normaltekst>Ekstern eierskap?</Normaltekst>
+            <div className="katergorioverskrift">
+                <Undertittel>Ekstern eierskap?</Undertittel>
             </div>
 
-            <div>
-                <Checkbox label={"Ja'"}/>
+            <div className="filtercheckbox">
+                <Checkbox label={"Ja"}/>
                 <Checkbox label={"Nei"}/>
             </div>
 
 
-            <div className="overskrift">
-                <Normaltekst>Offentlig Status</Normaltekst>
+            <div className="katergorioverskrift">
+                <Undertittel>Offentlig Status</Undertittel>
             </div>
 
-            <div>
-                <Checkbox label={"Ja'"}/>
+            <div className="filtercheckbox">
+                <Checkbox label={"Ja"}/>
                 <Checkbox label={"Nei"}/>
             </div>
 
