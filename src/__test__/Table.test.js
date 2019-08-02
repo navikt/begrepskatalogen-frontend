9b634329a330aba17d15d0085bd54c5772b06e80
Dennis <dennis.jianbin.liang@nav.no>
2019-08-02 13:58:44 +0200

    resolves weird giterror

--- src/__test__/Table.test.js
+++ src/__test__/Table.test.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { shallow } from "enzyme";
+
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Table from '../components/table/Table';
-Enzyme.configure({ adapter: new Adapter() });
 
-describe('Table component', ()=>{
+import configureStore from 'redux-mock-store';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
+import { Provider } from 'react-redux';
+
+import { Table } from '../components/table/Table';
+
+
+
+
+
+const mockStore = configureStore();
+const store = mockStore();
+
+
+describe('Table', ()=>{
+    it('matches snapshot', ()=>{
+        const renderer = new ShallowRenderer();
+        const result = renderer.render(
+            <Provider store = {store}>
+                <Table />
+            </Provider>
+        )
+        expect(result).toMatchSnapshot();
+    });
 })
+
+
