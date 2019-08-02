1cf77ac08599e4dca18f18f83f1a94389455be97
Eskil Hognestad <eskilhognestad@Eskils-MacBook-Pro.local>
2019-08-02 13:35:07 +0200

    added tests for omBegrepskatalogen

--- src/__test__/OmBegrepskatalogen.test.js
+++ src/__test__/OmBegrepskatalogen.test.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ShallowRenderer from "react-test-renderer/shallow";
 import { Provider } from "react-redux";
 import configureMockStore from "redux-mock-store";
-
+import Enzyme, { shallow } from 'enzyme';
 import OmBegrepskatalogen from '../pages/omBegrepskatalogen/OmBegrepskatalogen';
-
+import Adapter from 'enzyme-adapter-react-16';
 
 const mockStore = configureMockStore();
 const store = mockStore({});
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('omBegrepskatalogen', ()=>{
 
@@ -20,4 +21,27 @@ describe('omBegrepskatalogen', ()=>{
             );
         expect(result).toMatchSnapshot();
     });
+
+    describe('<OmBegrepskatalogen/>', () => {
+        it('renders one <h1> tag', ()=>{
+            const wrapper = shallow(<OmBegrepskatalogen/>);
+            expect(wrapper.find('h1')).toHaveLength(1);
+        })
+
+        it('renders one <div> tag', ()=>{
+            const wrapper = shallow(<OmBegrepskatalogen/>);
+            expect(wrapper.find('div')).toHaveLength(1);
+        })
+
+        it('renders one <p> tag', ()=>{
+            const wrapper = shallow(<OmBegrepskatalogen/>);
+            expect(wrapper.find('p')).toHaveLength(1);
+        })
+    })
+
+    
+    
+
+
 })
+
