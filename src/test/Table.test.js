4e59297e54a1943d23050fdf500bb79ac5cd4c29
Eskil Hognestad <eskilhognestad@Eskils-MacBook-Pro.local>
2019-08-02 09:34:52 +0200

    tests Header class

---
+++ src/test/Table.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from "enzyme";
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Table from '../components/table/Table';
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Table component', ()=>{
+
+})
