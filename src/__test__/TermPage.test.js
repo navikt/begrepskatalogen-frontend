846d351dfca815f017b6d436f91dfe824bf702f8
Eskil Hognestad <eskilhognestad@Eskils-MacBook-Pro.local>
2019-08-02 15:29:38 +0200

    termpage snapshot test

--- src/__test__/TermPage.test.js
+++ src/__test__/TermPage.test.js
@@ -4,14 +4,18 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import TermPage from '../pages/termpage/TermPage';
-Enzyme.configure({ adapter: new Adapter() });
+
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, { shallow } from 'enzyme';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 
 const mockStore = configureMockStore();
 const store = mockStore({});
 
 describe('TermPage', ()=>{
+
     it('matches snapshot', ()=>{
         const renderer = new ShallowRenderer();
         const result = renderer.render(
@@ -21,23 +25,6 @@ describe('TermPage', ()=>{
         )
         expect(result).toMatchSnapshot();
     });
-
-    
 })
-/*
-describe('<TermPage/>', () => {
-    it('renders one <h1> tag', ()=>{
-        const wrapper = shallow(<OmBegrepskatalogen/>);
-        expect(wrapper.find('h1')).toHaveLength(1);
-    })
 
-    it('renders one <div> tag', ()=>{
-        const wrapper = shallow(<OmBegrepskatalogen/>);
-        expect(wrapper.find('div')).toHaveLength(1);
-    })
-    
-    it('renders one <p> tag', ()=>{
-        const wrapper = shallow(<OmBegrepskatalogen/>);
-        expect(wrapper.find('p')).toHaveLength(1);
-    })
-})*/
+
