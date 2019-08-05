3dd4e3f1c982bfcd7b205ba7fb2e71802dd32c95
Thea Tokstad <33182871+theatok@users.noreply.github.com>
2019-08-05 09:04:00 +0200

    prefix to url (#112)

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -13,17 +13,17 @@ class Navigation extends React.Component{
                 <nav className="navStyle">
                     <ul className="navStyle__navLinks">
                         <li>
-                            <Link to={"/ombegrepskatalogen"} id="omBegrepeskatalog" className="navStyle__navLink lenke"> 
+                            <Link to={"/begrepskatalogen/ombegrepskatalogen"} id="omBegrepeskatalog" className="navStyle__navLink lenke"> 
                                 <Element>Om begrepskatalogen</Element>
                             </Link>
                         </li>
                         <li> 
-                            <Link to={"/kontaktoss"} id="kontaktOss" className="navStyle__navLink lenke">
+                            <Link to={"/begrepskatalogen/kontaktoss"} id="kontaktOss" className="navStyle__navLink lenke">
                                 <Element>Kontakt oss</Element>
                             </Link>
                         </li>
                         <li>  
-                            <Link to={'/meld-inn-nytt-begrep'} id="meldInnNyttBegrep" className="navStyle__navLink lenke">
+                            <Link to={'/begrepskatalogen/meld-inn-nytt-begrep'} id="meldInnNyttBegrep" className="navStyle__navLink lenke">
                                 <Element>Meld inn nytt begrep</Element>
                             </Link>
                         </li>
