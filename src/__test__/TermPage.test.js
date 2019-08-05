971230a9175265dd18a910c1ec2e3a06c6d8d803
Eskil Hognestad <eskilhognestad@Eskils-MacBook-Pro.local>
2019-08-05 13:58:07 +0200

    adds more testing.some of them not working..

--- src/__test__/TermPage.test.js
+++ src/__test__/TermPage.test.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import TermPage from '../pages/termpage/TermPage';
 import Adapter from 'enzyme-adapter-react-16';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -23,4 +23,39 @@ describe('TermPage', ()=>{
         expect(result).toMatchSnapshot();
     });
 })
+// dette fungerer ikke enda.. 
+describe('TermPage stats', () =>{
+    //const renderer = new ShallowRenderer();
+    const wrapper = shallow(
+        <Provider store = {store}>
+            <TermPage/>
+        </Provider>
+    );
+    it('has 13 div <tags> tags', () =>{
+
+        expect(wrapper.find('div')).toHaveLength(13);
+    });
+
+    it('has one <Sidetittel> tags', ()=>{
+        expect(wrapper.find('Sidetittel')).toHaveLength(1);
+    });
+
+    it('has one <Undertittel> tags', ()=>{
+        expect(wrapper.find('Undertittel')).toHaveLength(1);
+    });
+
+    it('has eight <Ingress> tags', ()=>{
+        expect(wrapper.find('Ingress')).toHaveLength(1);
+    });
+
+    it('has seven <Normaltekst> tags', ()=>{
+        expect(wrapper.find('Normaltekst')).toHaveLength(1);
+    });
+
+    it('has one <AlertStripeFeil> tags', ()=>{
+        expect(wrapper.find('Ingress')).toHaveLength(1);
+    });
+});
+
+
 
