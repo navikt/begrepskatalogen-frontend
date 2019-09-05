4d8cabc1c4fb549411d0ac7de0d5af87ffb11643
arshadkamran <arshad.ali.kamran@nav.no>
2019-09-05 14:19:23 +0200

    Oppdaterte Kontakt oss statisk innhold og less filen med info fra COnfluence

--- src/pages/kontaktoss/KontaktOss.js
+++ src/pages/kontaktoss/KontaktOss.js
@@ -3,10 +3,126 @@ import './KontaktOss.less';
 
 function KontaktOss() {
   return (
-      <div className="kontaktOss">
-        <h1>Kontakt oss</h1>
-        <p>Her kan du ta kontakt med oss i begrepskatalogen:))</p>
-      </div>
+    <div className="kontaktOss">
+      <h1>Kontakt oss</h1>
+      <p>Her kan du ta kontakt med oss i begrepskatalogen:))</p>
+
+      <div>
+        <h2> Ressurser og kontaktpersoner </h2>
+        <p>" "</p>
+        <NavTable columnWidths={[2, 2, 1]}>
+          <NavTableHead>
+            <NavTableHeadCell>
+              Navn
+        </NavTableHeadCell>
+            <NavTableHeadCell>
+              Rolle
+        </NavTableHeadCell>
+            <NavTableHeadCell>
+              e-post
+        </NavTableHeadCell>
+            <NavTableHeadCell>
+              Tilhørighet
+        </NavTableHeadCell>
+          </NavTableHead>
+          <NavTableBody>
+            <NavTableRow key={1}>
+              <NavTableCell>
+              Marte Vidnes Jensen
+            </NavTableCell>
+              <NavTableCell>
+              Ansvarlig for Begrepskatalogen i NAV
+            </NavTableCell>
+              <NavTableCell>
+              Marte.Vidnes.Jensen@nav.no
+            </NavTableCell>
+              <NavTableCell>
+              Ytelsesavdelingen - Seksjon informasjonsforvaltning
+        </NavTableCell>
+            </NavTableRow>
+
+            <NavTableRow key={2}>
+              <NavTableCell>
+              Silje Emilienne Aanderud-Larsen
+        </NavTableCell>
+              <NavTableCell>
+              Strategisk produkteier
+        </NavTableCell>
+              <NavTableCell>
+              Silje.Emilienne.Aanderud-Larsen@nav.no
+        </NavTableCell>
+              <NavTableCell>
+              Ytelsesavdelingen - Seksjon informasjonsforvaltning
+    </NavTableCell>
+            </NavTableRow>
+
+            <NavTableRow key={3}>
+              <NavTableCell>
+              Ingvild Alderslyst
+    </NavTableCell>
+              <NavTableCell>
+              Prosessdriver
+    </NavTableCell>
+              <NavTableCell>
+              Ingvild.Marie.Alderslyst@nav.no
+    </NavTableCell>
+              <NavTableCell>
+              NAV Registerforvaltning
+</NavTableCell>
+            </NavTableRow>
+
+            <NavTableRow key={4}>
+<NavTableCell>
+Rita Johansen
+</NavTableCell>
+            <NavTableCell>
+            Prosessdriver
+</NavTableCell>
+            <NavTableCell>
+            Rita.Johansen@nav.no
+</NavTableCell>
+            <NavTableCell>
+            NAV Registerforvaltning
+</NavTableCell>
+</NavTableRow>
+
+          <NavTableRow key={5}>
+            <NavTableCell>
+            Helle Ekern
+</NavTableCell>
+            <NavTableCell>
+            Prosessdriver
+</NavTableCell>
+            <NavTableCell>
+            Helle.Ekern@nav.no
+</NavTableCell>
+            <NavTableCell>
+            NAV Registerforvaltning
+</NavTableCell>
+          </NavTableRow>
+
+          <NavTableRow key={6}>
+            <NavTableCell>
+            Ken Ingvar Friskilæ
+</NavTableCell>
+            <NavTableCell>
+            Prosessdriver	
+</NavTableCell>
+            <NavTableCell>
+            Ken.Ingvar.Friskile@nav.no
+</NavTableCell>
+            <NavTableCell>
+            NAV Registerforvaltning
+</NavTableCell>
+          </NavTableRow>
+    </NavTableBody>
+</NavTable>
+
+    </div> 
+       </div > 
+
+        
+      </div >
   );
 }
 
