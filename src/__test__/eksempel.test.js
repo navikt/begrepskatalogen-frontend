c14263e9a558537e499b5c455c9fbbe644d745dc
arshadkamran <arshad.ali.kamran@nav.no>
2019-09-05 16:51:12 +0200

    Endret eksempel.test.js med en verdi på 6 isteden for 2

--- src/__test__/eksempel.test.js
+++ src/__test__/eksempel.test.js
@@ -16,5 +16,5 @@ test('App component testing', function() {
   const instance = component.root;
   const divs = instance.findByType('div');
   expect(divs.props.className).toBe('kontaktOss');
-  expect(divs.props.children).toHaveLength(2);
+  expect(divs.props.children).toHaveLength(6);
 });
